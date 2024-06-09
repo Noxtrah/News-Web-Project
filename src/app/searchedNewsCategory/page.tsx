00a70ecef2eaf5dfe189fc7c5f0498ff0a3ab3c4
Doğa Kayra Yılmazarslan <94742646+Noxtrah@users.noreply.github.com>
2024-06-09 15:13:05 +0300

    Added suspense

--- src/app/searchedNewsCategory/page.tsx
+++ src/app/searchedNewsCategory/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 import Image from 'next/image';
@@ -108,6 +108,7 @@ const SearchedNewsCategoryPage: React.FC = () => {
   };
 
   return (
+    <Suspense fallback={<div>Loading...</div>}> {/* Wrap the component with Suspense */}
     <div className="bg-gray-100 min-h-screen">
       <div className='p-2 md:px-8 lg:px-16 xl:px-32'>
       <Header className="px-4 sm:px-8 md:px-12 lg:px-18 xl:px-40" userProfilePicture={userProfilePicture} />
@@ -148,6 +149,7 @@ const SearchedNewsCategoryPage: React.FC = () => {
         </section>
       </div>
     </div>
+    </Suspense>
   );
 };
 
